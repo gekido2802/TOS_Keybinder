5967014557b189f4771b112970646aea95da8b3d
Gekido <gekido2802@gmail.com>
2015-11-08 16:40:32 -0500

    Implement Main Class
    Main is the root of the application and will choose the native look and
    field and send a new instance of JFrame to the FrameHelper

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+                e.printStackTrace();
+            }
+
+            FrameHelper.build(new JFrame());
+        });
     }
 }
